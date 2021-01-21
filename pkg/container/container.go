2e1bb9f39a6d6997c5e648bebb726d925f739d3a
sedhossein <seyedhossein.hosseini@snapp.cab>
2021-01-22 01:28:28 +0330

    substitute panics with error returning

--- pkg/container/container.go
+++ pkg/container/container.go
@@ -3,6 +3,7 @@
 package container
 
 import (
+	"errors"
 	"reflect"
 )
 
@@ -13,9 +14,9 @@ type binding struct {
 }
 
 // resolve will return the concrete of related abstraction.
-func (b binding) resolve(c Container) interface{} {
+func (b binding) resolve(c Container) (interface{}, error) {
 	if b.instance != nil {
-		return b.instance
+		return b.instance, nil
 	}
 
 	return c.invoke(b.resolver)
@@ -30,16 +31,20 @@ func NewContainer() Container {
 }
 
 // bind will map an abstraction to a concrete and set instance if it's a singleton binding.
-func (c Container) bind(resolver interface{}, singleton bool) {
+func (c Container) bind(resolver interface{}, singleton bool) error {
+	var err error
 	resolverTypeOf := reflect.TypeOf(resolver)
 	if resolverTypeOf.Kind() != reflect.Func {
-		panic("the resolver must be a function")
+		return errors.New("the resolver must be a function")
 	}
 
 	for i := 0; i < resolverTypeOf.NumOut(); i++ {
 		var instance interface{}
 		if singleton {
-			instance = c.invoke(resolver)
+			instance, err = c.invoke(resolver)
+			if err != nil {
+				return err
+			}
 		}
 
 		c[resolverTypeOf.Out(i)] = binding{
@@ -47,16 +52,24 @@ func (c Container) bind(resolver interface{}, singleton bool) {
 			instance: instance,
 		}
 	}
+
+	return nil
 }
 
 // invoke will call the given function and return its returned value.
 // It only works for functions that return a single value.
-func (c Container) invoke(function interface{}) interface{} {
-	return reflect.ValueOf(function).Call(c.arguments(function))[0].Interface()
+func (c Container) invoke(function interface{}) (interface{}, error) {
+	args, err := c.arguments(function)
+	if err != nil {
+		return nil, err
+	}
+
+	return reflect.ValueOf(function).Call(args)[0].Interface(), nil
 }
 
 // arguments will return resolved arguments of the given function.
-func (c Container) arguments(function interface{}) []reflect.Value {
+func (c Container) arguments(function interface{}) ([]reflect.Value, error) {
+	var err error
 	functionTypeOf := reflect.TypeOf(function)
 	argumentsCount := functionTypeOf.NumIn()
 	arguments := make([]reflect.Value, argumentsCount)
@@ -67,29 +80,32 @@ func (c Container) arguments(function interface{}) []reflect.Value {
 		var instance interface{}
 
 		if concrete, ok := c[abstraction]; ok {
-			instance = concrete.resolve(c)
+			instance, err = concrete.resolve(c)
+			if err != nil {
+				return nil, err
+			}
 		} else {
-			panic("no concrete found for the abstraction: " + abstraction.String())
+			return nil, errors.New("no concrete found for the abstraction: " + abstraction.String())
 		}
 
 		arguments[i] = reflect.ValueOf(instance)
 	}
 
-	return arguments
+	return arguments, nil
 }
 
 // Singleton will bind an abstraction to a concrete for further singleton resolves.
 // It takes a resolver function which returns the concrete and its return type matches the abstraction (interface).
 // The resolver function can have arguments of abstraction that have bound already in Container.
-func (c Container) Singleton(resolver interface{}) {
-	c.bind(resolver, true)
+func (c Container) Singleton(resolver interface{}) error {
+	return c.bind(resolver, true)
 }
 
 // Transient will bind an abstraction to a concrete for further transient resolves.
 // It takes a resolver function which returns the concrete and its return type matches the abstraction (interface).
 // The resolver function can have arguments of abstraction that have bound already in Container.
-func (c Container) Transient(resolver interface{}) {
-	c.bind(resolver, false)
+func (c Container) Transient(resolver interface{}) error {
+	return c.bind(resolver, false)
 }
 
 // Reset will reset the container and remove all the bindings.
@@ -103,29 +119,35 @@ func (c Container) Reset() {
 // It can take an abstraction (interface reference) and fill it with the related implementation.
 // It also can takes a function (receiver) with one or more arguments of the abstractions (interfaces) that need to be
 // resolved, Container will invoke the receiver function and pass the related implementations.
-func (c Container) Make(receiver interface{}) {
+func (c Container) Make(receiver interface{}) error {
 	receiverTypeOf := reflect.TypeOf(receiver)
 	if receiverTypeOf == nil {
-		panic("cannot detect type of the receiver, make sure your are passing reference of the object")
+		return errors.New("cannot detect type of the receiver, make sure your are passing reference of the object")
 	}
 
 	if receiverTypeOf.Kind() == reflect.Ptr {
 		abstraction := receiverTypeOf.Elem()
 
 		if concrete, ok := c[abstraction]; ok {
-			instance := concrete.resolve(c)
+			instance, err := concrete.resolve(c)
+			if err != nil {
+				return err
+			}
 			reflect.ValueOf(receiver).Elem().Set(reflect.ValueOf(instance))
-			return
+			return nil
 		}
 
-		panic("no concrete found for the abstraction " + abstraction.String())
+		return errors.New("no concrete found for the abstraction " + abstraction.String())
 	}
 
 	if receiverTypeOf.Kind() == reflect.Func {
-		arguments := c.arguments(receiver)
+		arguments, err := c.arguments(receiver)
+		if err != nil {
+			return err
+		}
 		reflect.ValueOf(receiver).Call(arguments)
-		return
+		return nil
 	}
 
-	panic("the receiver must be either a reference or a callback")
+	return errors.New("the receiver must be either a reference or a callback")
 }
