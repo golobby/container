b6f029ed8e6dc2fde937d1577d96df05f8535dd9
Milad Rahimi <info@miladrahimi.com>
2021-02-21 22:21:54 +0330

    clean up tests

--- pkg/container/container_test.go
+++ pkg/container/container_test.go
@@ -37,111 +37,61 @@ func (m MySQL) Connect() bool {
 
 var instance = container.NewContainer()
 
-func TestSingletonItShouldMakeAnInstanceOfTheAbstraction(t *testing.T) {
-	area := 5
-
+func TestContainer_Singleton(t *testing.T) {
 	err := instance.Singleton(func() Shape {
-		return &Circle{a: area}
+		return &Circle{a: 13}
 	})
 	assert.NoError(t, err)
-
-	err = instance.Make(func(s Shape) {
-		a := s.GetArea()
-		assert.Equal(t, area, a)
-	})
-	assert.NoError(t, err)
-}
-
-func TestSingletonItShouldMakeSameObjectEachMake(t *testing.T) {
-	err := instance.Singleton(func() Shape {
-		return &Circle{a: 5}
-	})
-	assert.NoError(t, err)
-
-	area := 6
 
 	err = instance.Make(func(s1 Shape) {
-		s1.SetArea(area)
+		s1.SetArea(666)
 	})
 	assert.NoError(t, err)
 
 	err = instance.Make(func(s2 Shape) {
 		a := s2.GetArea()
-		assert.Equal(t, a, area)
+		assert.Equal(t, a, 666)
 	})
 	assert.NoError(t, err)
 }
 
-func TestSingletonWithNonFunctionResolverItShouldPanic(t *testing.T) {
-	expectedErr := "the resolver must be a function"
+func TestContainer_Singleton_With_NonFunction_Resolver_It_Should_Fail(t *testing.T) {
 	err := instance.Singleton("STRING!")
-	assert.EqualError(t, err, expectedErr)
+	assert.EqualError(t, err, "the resolver must be a function")
 }
 
-func TestSingletonItShouldResolveResolverArguments(t *testing.T) {
-	area := 5
+func TestContainer_Singleton_With_Resolvable_Arguments(t *testing.T) {
 	err := instance.Singleton(func() Shape {
-		return &Circle{a: area}
+		return &Circle{a: 666}
 	})
 	assert.NoError(t, err)
 
 	err = instance.Singleton(func(s Shape) Database {
-		assert.Equal(t, s.GetArea(), area)
+		assert.Equal(t, s.GetArea(), 666)
 		return &MySQL{}
 	})
 	assert.NoError(t, err)
 }
 
-func TestTransientItShouldMakeDifferentObjectsOnMake(t *testing.T) {
-	area := 5
-
+func TestContainer_Transient(t *testing.T) {
 	err := instance.Transient(func() Shape {
-		return &Circle{a: area}
+		return &Circle{a: 666}
 	})
 	assert.NoError(t, err)
 
 	err = instance.Make(func(s1 Shape) {
-		s1.SetArea(6)
+		s1.SetArea(13)
 	})
 	assert.NoError(t, err)
 
 	err = instance.Make(func(s2 Shape) {
 		a := s2.GetArea()
-		assert.Equal(t, a, area)
+		assert.Equal(t, a, 666)
 	})
 	assert.NoError(t, err)
 }
 
-func TestTransientItShouldMakeAnInstanceOfTheAbstraction(t *testing.T) {
-	area := 5
-
-	err := instance.Transient(func() Shape {
-		return &Circle{a: area}
-	})
-	assert.NoError(t, err)
-
-	err = instance.Make(func(s Shape) {
-		a := s.GetArea()
-		assert.Equal(t, a, area)
-	})
-	assert.NoError(t, err)
-}
-
-func TestMakeWithSingleInputAndCallback(t *testing.T) {
-	err := instance.Singleton(func() Shape {
-		return &Circle{a: 5}
-	})
-	assert.NoError(t, err)
-
-	err = instance.Make(func(s Shape) {
-		if _, ok := s.(*Circle); !ok {
-			t.Error("Expected Circle")
-		}
-	})
-	assert.NoError(t, err)
-}
-
-func TestMakeWithMultipleInputsAndCallback(t *testing.T) {
+func TestContainer_Make_With_Multiple_Resolving(t *testing.T) {
 	err := instance.Singleton(func() Shape {
 		return &Circle{a: 5}
 	})
@@ -164,23 +114,7 @@ func TestMakeWithMultipleInputsAndCallback(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func TestMakeWithSingleInputAndReference(t *testing.T) {
-	err := instance.Singleton(func() Shape {
-		return &Circle{a: 5}
-	})
-	assert.NoError(t, err)
-
-	var s Shape
-
-	err = instance.Make(&s)
-	assert.NoError(t, err)
-
-	if _, ok := s.(*Circle); !ok {
-		t.Error("Expected Circle")
-	}
-}
-
-func TestMakeWithMultipleInputsAndReference(t *testing.T) {
+func TestContainer_Make_With_Reference_As_Resolver(t *testing.T) {
 	err := instance.Singleton(func() Shape {
 		return &Circle{a: 5}
 	})
@@ -209,31 +143,26 @@ func TestMakeWithMultipleInputsAndReference(t *testing.T) {
 	}
 }
 
-func TestMakeWithUnsupportedReceiver(t *testing.T) {
-	expectedErr := "the receiver must be either a reference or a callback"
+func TestContainer_Make_With_Unsupported_Receiver_It_Should_Fail(t *testing.T) {
 	err := instance.Make("STRING!")
-	assert.EqualError(t, err, expectedErr)
+	assert.EqualError(t, err, "the receiver must be either a reference or a callback")
 }
 
-func TestMakeWithNonReference(t *testing.T) {
-	expectedErr := "cannot detect type of the receiver"
+func TestContainer_Make_With_NonReference_Receiver_It_Should_Fail(t *testing.T) {
 	var s Shape
-
 	err := instance.Make(s)
-	assert.EqualError(t, err, expectedErr)
+	assert.EqualError(t, err, "cannot detect type of the receiver")
 }
 
-func TestMakeWithUnboundedAbstraction(t *testing.T) {
-	expectedErr := "no concrete found for the abstraction: container_test.Shape"
-	var s Shape
+func TestContainer_Make_With_UnBounded_Reference_It_Should_Fail(t *testing.T) {
 	instance.Reset()
 
+	var s Shape
 	err := instance.Make(&s)
-	assert.EqualError(t, err, expectedErr)
+	assert.EqualError(t, err, "no concrete found for the abstraction: container_test.Shape")
 }
 
-func TestMakeWithCallbackThatHasAUnboundedAbstraction(t *testing.T) {
-	expectedErr := "no concrete found for the abstraction: container_test.Database"
+func TestContainer_Make_With_Second_UnBounded_Argument(t *testing.T) {
 	instance.Reset()
 
 	err := instance.Singleton(func() Shape {
@@ -242,5 +171,5 @@ func TestMakeWithCallbackThatHasAUnboundedAbstraction(t *testing.T) {
 	assert.NoError(t, err)
 
 	err = instance.Make(func(s Shape, d Database) {})
-	assert.EqualError(t, err, expectedErr)
+	assert.EqualError(t, err, "no concrete found for the abstraction: container_test.Database")
 }
