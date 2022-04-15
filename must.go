b8d27846d0e694c0449a5f51538699867b2dd338
Milad Rahimi <realmiladrahimi@gmail.com>
2022-04-15 19:41:54 +0430

    add test for must helpers

--- must.go
+++ must.go
@@ -1,31 +1,57 @@
 package container
 
+// MustSingleton wraps the `Singleton` method and panics on errors instead of returning the errors.
 func MustSingleton(c Container, resolver interface{}) {
 	if err := c.Singleton(resolver); err != nil {
 		panic(err)
 	}
 }
 
+// MustNamedSingleton wraps the `NamedSingleton` method and panics on errors instead of returning the errors.
 func MustNamedSingleton(c Container, name string, resolver interface{}) {
 	if err := c.NamedSingleton(name, resolver); err != nil {
 		panic(err)
 	}
 }
 
+// MustTransient wraps the `Transient` method and panics on errors instead of returning the errors.
 func MustTransient(c Container, resolver interface{}) {
 	if err := c.Transient(resolver); err != nil {
 		panic(err)
 	}
 }
 
+// MustNamedTransient wraps the `NamedTransient` method and panics on errors instead of returning the errors.
 func MustNamedTransient(c Container, name string, resolver interface{}) {
 	if err := c.NamedTransient(name, resolver); err != nil {
 		panic(err)
 	}
 }
 
+// MustCall wraps the `Call` method and panics on errors instead of returning the errors.
 func MustCall(c Container, receiver interface{}) {
 	if err := c.Call(receiver); err != nil {
+		panic(err)
+	}
+}
+
+// MustResolve wraps the `Resolve` method and panics on errors instead of returning the errors.
+func MustResolve(c Container, abstraction interface{}) {
+	if err := c.Resolve(abstraction); err != nil {
+		panic(err)
+	}
+}
+
+// MustNamedResolve wraps the `NamedResolve` method and panics on errors instead of returning the errors.
+func MustNamedResolve(c Container, abstraction interface{}, name string) {
+	if err := c.NamedResolve(abstraction, name); err != nil {
+		panic(err)
+	}
+}
+
+// MustFill wraps the `Fill` method and panics on errors instead of returning the errors.
+func MustFill(c Container, receiver interface{}) {
+	if err := c.Fill(receiver); err != nil {
 		panic(err)
 	}
 }
---
+++ must_test.go
@@ -0,0 +1,89 @@
+package container_test
+
+import (
+	"errors"
+	"github.com/golobby/container/v3"
+	"testing"
+)
+
+func TestMustSingleton_It_Should_Panic_On_Error(t *testing.T) {
+	c := container.New()
+
+	defer func() { recover() }()
+	container.MustSingleton(c, func() (Shape, error) {
+		return nil, errors.New("error")
+	})
+	t.Errorf("panic expcted.")
+}
+
+func TestMustNamedSingleton_It_Should_Panic_On_Error(t *testing.T) {
+	c := container.New()
+
+	defer func() { recover() }()
+	container.MustNamedSingleton(c, "name", func() (Shape, error) {
+		return nil, errors.New("error")
+	})
+	t.Errorf("panic expcted.")
+}
+
+func TestMustTransient_It_Should_Panic_On_Error(t *testing.T) {
+	c := container.New()
+
+	defer func() { recover() }()
+	container.MustTransient(c, func() (Shape, error) {
+		return nil, errors.New("error")
+	})
+	t.Errorf("panic expcted.")
+}
+
+func TestMustNamedTransient_It_Should_Panic_On_Error(t *testing.T) {
+	c := container.New()
+
+	defer func() { recover() }()
+	container.MustNamedTransient(c, "name", func() (Shape, error) {
+		return nil, errors.New("error")
+	})
+	t.Errorf("panic expcted.")
+}
+
+func TestMustCall_It_Should_Panic_On_Error(t *testing.T) {
+	c := container.New()
+
+	defer func() { recover() }()
+	container.MustCall(c, func(s Shape) {
+		s.GetArea()
+	})
+	t.Errorf("panic expcted.")
+}
+
+func TestMustResolve_It_Should_Panic_On_Error(t *testing.T) {
+	c := container.New()
+
+	var s Shape
+
+	defer func() { recover() }()
+	container.MustResolve(c, &s)
+	t.Errorf("panic expcted.")
+}
+
+func TestMustNamedResolve_It_Should_Panic_On_Error(t *testing.T) {
+	c := container.New()
+
+	var s Shape
+
+	defer func() { recover() }()
+	container.MustNamedResolve(c, &s, "name")
+	t.Errorf("panic expcted.")
+}
+
+func TestMustFill_It_Should_Panic_On_Error(t *testing.T) {
+	c := container.New()
+
+	myApp := struct {
+		S Shape `container:"type"`
+	}{}
+
+	defer func() { recover() }()
+	container.MustFill(c, &myApp)
+	t.Errorf("panic expcted.")
+}
